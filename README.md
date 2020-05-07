a51301c4d1f58289e67d2a90989989d372cdd4c1
Alan Thai <alan.thai@shopify.com>
2020-05-07 13:56:24 -0400

    Fix generator script

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Setup
 
-```
+```bin
 npm install
 
 # See list of available extension types: `scripts/generate/constants.ts`
-npm run generate --type=SUBSCRIPTIONS_MANAGEMENT
+npm run generate -- --type=SUBSCRIPTIONS_MANAGEMENT
+```
+
+via yarn
+
+```bin
+yarn
+
+# See list of available extension types: `scripts/generate/constants.ts`
+yarn generate --type=SUBSCRIPTIONS_MANAGEMENT
 ```
--- scripts/generate/index.ts
+++ scripts/generate/index.ts
@@ -4,15 +4,15 @@ import {generateSrc} from './generate-src';
 import {generateConfig} from './generate-config';
 import {extensionTypeToPoint} from './constants';
 
-const {type} = yargs.command<{type: string}>(
-  'type',
-  'Extension type identifier'
-).argv;
+const {type} = yargs.option('type', {
+  type: 'string',
+  description: 'Extension type identifier',
+}).argv;
 
-let extensionPoint = extensionTypeToPoint[type];
+let extensionPoint = extensionTypeToPoint[type!];
 if (!extensionPoint) {
   console.warn(
-    'Warning: Unknown extension point ${type}, using SubscriptionsManagement instead'
+    `Warning: Unknown extension point ${type}, using SubscriptionsManagement instead`
   );
   extensionPoint = ExtensionPoint.SubscriptionsManagement;
 }
