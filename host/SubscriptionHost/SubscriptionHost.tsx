5c91eb7cf4055b33482507d989b329b8fd5775ca
Alan Thai <alan.thai@shopify.com>
2020-05-13 15:16:22 -0400

    feat: fake product page (#17)
    feat: fake product page

---
+++ host/SubscriptionHost/SubscriptionHost.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {ExtensionPoint} from '@shopify/argo';
+import {Button, Navigation, Frame, TopBar} from '@shopify/polaris';
+import {
+  ProductsMajorTwotone,
+  SettingsMajorMonotone,
+} from '@shopify/polaris-icons';
+
+import {ModalContainer} from '../containers/ModalContainer';
+import {HostProps} from '../types';
+import {SubscriptionExtension} from './SubscriptionExtension';
+
+export function SubscriptionHost(props: HostProps) {
+  const [open, setOpen] = useState(false);
+
+  const navigationMarkup = (
+    <Navigation location="/">
+      <Navigation.Section
+        fill
+        items={[
+          {
+            url: '',
+            label: 'Subscription extension',
+            icon: ProductsMajorTwotone,
+            selected: true,
+          },
+        ]}
+      />
+      <Navigation.Section
+        items={[
+          {
+            url: '/settings',
+            label: 'Settings',
+            icon: SettingsMajorMonotone,
+          },
+        ]}
+      />
+    </Navigation>
+  );
+
+  return (
+    <Frame navigation={navigationMarkup} topBar={<TopBar />}>
+      <SubscriptionExtension>
+        <Button onClick={() => setOpen(true)}>Create subscription plan</Button>
+        <ModalContainer
+          app={{name: 'App name', appId: 'app-id'}}
+          open={open}
+          defaultTitle="Default title"
+          onClose={() => setOpen(false)}
+          extensionPoint={ExtensionPoint.SubscriptionManagement}
+          {...props}
+        />
+      </SubscriptionExtension>
+    </Frame>
+  );
+}
---
+++ host/SubscriptionHost/index.ts
@@ -0,0 +1,1 @@
+export {SubscriptionHost} from './SubscriptionHost';
